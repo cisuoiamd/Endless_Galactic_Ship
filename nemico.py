f2845ca8e21efbe751731e586091d6d33e747c64
Ezuriii <137899809+cisuoiamd@users.noreply.github.com>
2026-02-14 10:46:55 +0100

    Remove Enemy class from nemico.py

--- nemico.py
+++ nemico.py
@@ -1,15 +1,1 @@
-import arcade
-import random
 
-class Enemy(arcade.Sprite):
-    def __init__(self, screen_height, scale=0.55, speed=4.2):
-        super().__init__("./assets/nemico.png", scale=scale)
-        self.center_x = 950
-        self.center_y = random.randint(50, screen_height - 50)
-        self.change_x = -speed
-        self.change_y = 0
-
-    def update(self):
-        super().update()
-        if self.right < 0:
-            self.remove_from_sprite_lists()
